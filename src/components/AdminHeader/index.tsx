ca7b35fdaa22874e5b1924fada3e6708ef7ebb5b
Druthvik7 <107387477+Druthvik7@users.noreply.github.com>
2023-04-29 18:44:49 +0530

    header

--- src/components/AdminHeader/index.tsx
+++ src/components/AdminHeader/index.tsx
@@ -1,45 +1,43 @@
-import React, { Component } from "react";
-import { Routes, Route, Link, useNavigate } from "react-router-dom";
-import "bootstrap/dist/css/bootstrap.min.css";
-import { Logout } from "tabler-icons-react";
+import React from 'react'
+import { Link, useNavigate } from 'react-router-dom'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import { Logout } from 'tabler-icons-react'
 
-const AdminHeader=()=>  {
-  const navigate = useNavigate();
+const AdminHeader = () => {
+  const navigate = useNavigate()
 
   function logout () {
-    localStorage.removeItem('@cinimaDb:Token');
-    localStorage.removeItem('authuser');
-    navigate("/admin/login");
-    
+    localStorage.removeItem('@cinimaDb:Token')
+    localStorage.removeItem('authuser')
+    navigate('/admin/login')
   };
 
-    return (
+  return (
       <div>
         <nav className="navbar navbar-expand navbar-dark bg-dark">
-          <Link to={"/"} className="navbar-brand">
+          <Link to={'/'} className="navbar-brand">
             CinemaDB
           </Link>
           <div className="navbar-nav mr-auto">
             <li className="nav-item">
-                <Link to={"/admin/userListing"} className="nav-link">
-                  User 
+                <Link to={'/admin/userListing'} className="nav-link">
+                  User
                 </Link>
               </li>
-              {/* <li className="nav-item">
-                <Link to={"/admin/dynamicForms"} className="nav-link">
-                  Form
-                </Link>
-               
-              </li> */}
               <li id="professional" className="nav-item">
-                 <Link to={"/admin/professionalListing"} className="nav-link">
+                 <Link to={'/admin/professionalListing'} className="nav-link">
                   Professional
+                  </Link>
+                  </li>
+              <li id="professional" className="nav-item">
+                 <Link to={'/admin/formlisitng'} className="nav-link">
+                  Form Listing
                   </Link>
                   </li>
               <li className="text-right">
                <Logout
                   className=" position-absolute top-8 end-0 translate-middle pointer "
-                  onClick={logout} 
+                  onClick={logout}
                   size={30}
                   strokeWidth={2}
                   color={'#FFFFFF'}
@@ -48,9 +46,8 @@ const AdminHeader=()=>  {
           </div>
         </nav>
       </div>
-      
-     );
-  }
 
+  )
+}
 
-export default AdminHeader;
+export default AdminHeader
