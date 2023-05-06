bf613c3b163a26da43d30a6ae5030825941f746f
Druthvik7 <107387477+Druthvik7@users.noreply.github.com>
2023-05-06 23:47:22 +0530

    As per the logic i have coded and also i have to make some changes. Those changes are written as a comment above methods which has to be changed

--- src/pages/Film/Register/Filmpersonregister/subcategoryuserForm.tsx
+++ src/pages/Film/Register/Filmpersonregister/subcategoryuserForm.tsx
@@ -13,7 +13,7 @@ import { environment } from '../../../../config/environment'
 import { Tabs } from 'antd'
 import { ReactFormGenerator } from 'react-form-builder2'
 import RegistrationHeader from '../../../../components/RegisterationHeader/registrationheader'
-import { getBreadCrumbs, getTitleFromTabs } from '../../../../services/filmservices'
+import { getTitleFromTabs } from '../../../../services/filmservices'
 interface InputData {
     user
     selectednodes
@@ -24,19 +24,21 @@ interface Tab {
 }
 
 const displayTabs: Tab[] = []
-let breadCrumbPathLists: string[] = []
+let currentSubCategoryType: any
+let currentSubCategory: string = ''
 export const SubCategoryUserForm: React.FC = () => {
   const inputData = useLocation().state as InputData
-  const [formGeneratorData, setformGeneratorData] = React.useState<any[]>([])
-  let path = ''
-  const selectedTabIndex = 0
+  const [selectedMastersOfTheCurrentSubCategory, setSelectedMastersOfTheCurrentSubCategory] = React.useState([])
+  const [formGeneratorLayoutOfSelectedTabAndType, setFormGeneratorLayoutOfSelectedTabAndType] = React.useState()
+  const [formUserProfessionData, setFormUserProfessionData] = React.useState<any[]>([])
+
   useEffect(() => {
     retriveTabs()
   }, [])
 
   const retriveTabs = async () => {
     const userdata = inputData.user
-    userdata.UserSubCategory.map((item) => {
+    userdata.userSubCategory.map((item) => {
       item.value.map((item) => {
         displayTabs.push({
           key: item.key,
@@ -44,33 +46,66 @@ export const SubCategoryUserForm: React.FC = () => {
         })
       })
     })
+    currentSubCategory = displayTabs[0].label
+    loadSubCategoryTypes(currentSubCategory)
+    currentSubCategoryType = selectedMastersOfTheCurrentSubCategory[0]
   }
-  const loadDataFromBE = async (key: string) => {
-    const newLabelPath = path.replace(/\s+/g, '').toLowerCase()
-    const mainLablePath = newLabelPath.replace('/', '').toLocaleLowerCase()
-    const serverData = await api
-      .get(`form/readfile/${environment.formLayoutPath}/${mainLablePath}/${environment.professionalData}`)
-    const response = await serverData.data
-    setformGeneratorData(response)
+  // load vertical menu
+  /* for the vertical menu same antd component to render it we want both key and label as per
+ the documentation like same as the tab bar but should change the position */
+  // https://stackblitz.com/run?file=demo.tsx
+  const loadSubCategoryTypes = async (currentSubCategory) => {
+    const selectedMastersOfTheCurrentSubCategory = await api.get(`form/readfile/formlayout/${currentSubCategory}/${environment.professionalData}`)
+    setSelectedMastersOfTheCurrentSubCategory(await selectedMastersOfTheCurrentSubCategory.data)
+  }
+  // load both tabs at same time
+  const loadFormGeneratorAndUserProfessionData = async (currentSubCategory, currentSubCategoryType) => {
+    const formGeneratorLayoutOfSelectedTabAndType = await api.get(`form/readfile/mastertemplates/${currentSubCategoryType}/${environment.professionalData}`)
+    setFormGeneratorLayoutOfSelectedTabAndType(await formGeneratorLayoutOfSelectedTabAndType.data)
+    const formUserProfessionData = await api.get(`userprofession/formdata/${inputData.user.id}/${currentSubCategory}/${currentSubCategoryType}`)
+    setFormUserProfessionData(await formUserProfessionData.data)
   }
 
-  const handleOnClickTitle = (key: string) => {
-    const tabs = getTitleFromTabs(key, displayTabs)
-    path = tabs
-    breadCrumbPathLists = getBreadCrumbs(key, displayTabs)
-    loadDataFromBE(key)
+  // vertical bar onclick
+  /* Here i cant able to get the title of tab so i will use key and call (getTitleFromTabs) which is in
+ ile service file */
+  const onClickOfSubCategoryType = (selectedSubCategoryType : string) => {
+    currentSubCategoryType = selectedSubCategoryType
+    loadFormGeneratorAndUserProfessionData(currentSubCategory, currentSubCategoryType)
+  }
+  // horizontal bar on click
+  /* Here i cant able to get the title of tab so i will use key and call (getTitleFromTabs) which is in
+ ile service file */
+  const onClickOfSubCategoryTab = (selectedSubCategory : string) => {
+    currentSubCategory = selectedSubCategory
+    loadSubCategoryTypes(currentSubCategory)
+    currentSubCategoryType = selectedMastersOfTheCurrentSubCategory[0]
+    loadFormGeneratorAndUserProfessionData(currentSubCategory, currentSubCategoryType)
+  }
+
+  // on save should call post api
+  const onClickOfSave = () => {
+    if (formUserProfessionData === undefined) {
+      formUserProfessionData.userId = inputData.user.id,
+      formUserProfessionData.subCategory = currentSubCategory,
+      formUserProfessionData.subCategoryType = currentSubCategoryType
+
+      // formdatajson - is value from submit of form
+      formUserProfessionData.value = formDataJson
+      api.post('userprofession/formdata​​​​', formUserProfessionData)
+    }
   }
   return (
     <>
    <div className="bg-white_A700 flex flex-col items-center justify-start mx-auto pb-7 w-full">
-        <div className="bg-bluegray_101 flex font-montserrat sm:px-5 shadow-bs3 w-full">
+        <div className="bg-bluegray_101 flex font-montserrat sm:px-5 shadow-bs w-full">
           <RegistrationHeader/>
         </div>
         <div className="flex font-montserrat items-start mt-[10px] md:px-10 sm:px-5 px-[78px] w-full">
           <div className="flex items-center justify-start w-[74%] md:w-full">
             <div className="flex flex-col items-center justify-start w-full">
               <div className="flex md:flex-col items-start w-full">
-                 <Tabs defaultActiveKey="1" items={displayTabs} onChange={handleOnClickTitle}/>
+                 <Tabs defaultActiveKey="1" items={displayTabs}/>
               </div>
               <div className="h-[3px] relative w-full">
               </div>
@@ -88,12 +123,12 @@ export const SubCategoryUserForm: React.FC = () => {
               </div>
 
               <div className="flex md:flex-col flex-row md:gap-5 items-center justify-start my-0 w-[93%] md:w-full">
-                 {formGeneratorData.length > 0 &&
+                 {formUserProfessionData.length > 0 &&
                     <ReactFormGenerator
                     back_action=""
                     form_action=""
                     form_method="POST"
-                    data={formGeneratorData}
+                    data={formUserProfessionData}
                   />
                   }
               </div>
