50cdc8520d909227b040897f3c317068d4272a3c
Druthvik7 <107387477+Druthvik7@users.noreply.github.com>
2023-05-26 10:59:57 +0530

    While login token was not taking by headers because of request interceptor so i added token to request interceptor now its working according to what we want

--- src/services/api.ts
+++ src/services/api.ts
@@ -3,15 +3,18 @@ import axios from 'axios'
 import { toast } from 'react-toastify'
 import { environment } from '../config/environment'
 
+const token = localStorage.getItem('@cinimaDb:Token');
 export const api = axios.create({
   baseURL: environment.baseUrl,
+  timeout: 5000,
   headers: {
-    Authorization: `Bearer ${localStorage.getItem('@cinimaDb:Token')}`,
+    Authorization: `Bearer ${token}`,
     'Content-type': 'application/json'
   }
 })
 
 api.interceptors.request.use((config) => {
+  config.headers!.Authorization = `Bearer ${token}`;
   toast('Loading...', {
     toastId: 'APP_LOADING',
     isLoading: true,
