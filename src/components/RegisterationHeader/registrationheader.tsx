ca6b3809d26aa3eb88d4c2adc9463c6f8ec20d6b
Druthvik7 <107387477+Druthvik7@users.noreply.github.com>
2023-06-02 19:45:24 +0530

    header back commit

--- src/components/RegisterationHeader/registrationheader.tsx
+++ src/components/RegisterationHeader/registrationheader.tsx
@@ -2,8 +2,11 @@
 import { useNavigate } from 'react-router-dom'
 import { MathLower, Logout } from 'tabler-icons-react'
 import { Text } from '../Elements'
+import { Modal, Button } from 'react-bootstrap'
+import React from 'react'
 const RegistrationHeader = () => {
   const navigate = useNavigate()
+  const [isShow, invokeModal] = React.useState(false)
 
   function logout () {
     localStorage.removeItem('@cinimaDb:Token')
@@ -14,39 +17,49 @@ const RegistrationHeader = () => {
   const back = () => {
     if (window.location.pathname.includes('selectedindustry')) {
       navigate('/film/register/filmpersonregister')
-    }
-    if (window.location.pathname.includes('filmpersonregister')) {
-      navigate('/film/login/loginregister')
-    }
-    if (window.location.pathname.includes('loginregister')) {
-      navigate('/film/login/selectpreference')
+    } else if (window.location.pathname.includes('filmpersonregister')) {
+      modalOn()
     }
   }
+
+  const modalOn = () => {
+    return invokeModal(!false)
+  }
+  const modalOff = () => {
+    return invokeModal(false)
+  }
   return (
-      <>
+    <>
       <div className="flex items-center justify-start md:ml-[0] ml-[70px] md:px-5 w-[9%] md:w-full">
-              <div className="flex flex-row gap-7 items-center justify-start w-full">
-              <MathLower
-               size={50}
-               strokeWidth={2}
-               color={'black'}
-               />
-                <Text
-                  className="font-medium text-black_900"
-                  variant="body11"
-                  onClick={back}
-                >
-                  Back
-                </Text>
-              </div>
-            </div>
-            <Logout
-                  className=" position-absolute top-8 end-0 translate-middle pointer "
-                  onClick={logout}
-                  size={30}
-                  strokeWidth={2}
-                  color={'#000000'}
-                 />;
+        <div className="flex flex-row gap-7 items-center justify-start w-full">
+          <MathLower
+            size={50}
+            strokeWidth={2}
+            color={'black'}
+          />
+          <Text
+            className="font-medium text-black_900"
+            variant="body11"
+            onClick={back}
+          >
+            Back
+          </Text>
+        </div>
+      </div>
+      <Modal show={isShow} onHide={() => modalOn()}>
+          <Modal.Body>Are You Sure You Want To Logout?</Modal.Body>
+          <Modal.Footer>
+            <Button onClick={() => modalOff()}>No</Button>
+            <Button className='btn btn-danger' onClick={() => logout()}>Yes</Button>
+          </Modal.Footer>
+      </Modal>
+      <Logout
+        className=" position-absolute top-8 end-0 translate-middle pointer "
+        onClick={modalOn}
+        size={30}
+        strokeWidth={2}
+        color={'#000000'}
+      />
       </>
 
   )
