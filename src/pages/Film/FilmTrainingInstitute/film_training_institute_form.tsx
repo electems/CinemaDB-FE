dfd858f16b466098b090dcd7643d4eb325f5f146
harsha@electems.com <harsha@electems.com>
2023-07-04 13:34:51 +0530

    popup screen code commit

--- src/pages/Film/FilmTrainingInstitute/film_training_institute_form.tsx
+++ src/pages/Film/FilmTrainingInstitute/film_training_institute_form.tsx
@@ -38,6 +38,7 @@ let fileRecord: any;
 const FilmInstituteRegistration: React.FC = () => {
   const [isReadOnly, setIsReadOnly] = React.useState(true)
   const loggedInUser = storage.getLoggedUser();
+  const [showSuccessfullPop, setshowSuccessfullPop] = React.useState(false)
   const [filmInstitute, setFilmInstitute] =
     React.useState(initialFilmInstitute);
   const navigate = useNavigate();
@@ -115,7 +116,10 @@ const FilmInstituteRegistration: React.FC = () => {
     }
     await api.post('/filminsitutetraining/filmInstituteTraining/notification', filmInstituteNotification)
 
-    navigate('/film/filminstitutetraining/traininginstitutes')
+    if(filmInstituteRecord.data != null && filmInstituteRecord.statusText === 'Created'){
+      btnClick()
+    }
+    //navigate('/film/filminstitutetraining/traininginstitutes')
   };
 
   const dropdownValues = [
@@ -132,6 +136,11 @@ const FilmInstituteRegistration: React.FC = () => {
       value = path
     }
   };
+
+   const btnClick =  async() =>{
+    setshowSuccessfullPop(true)
+   }
+
 
   return (
     <>
@@ -617,10 +626,62 @@ const FilmInstituteRegistration: React.FC = () => {
                     </Button>
                   </div>
                 </div>
+                  {/* pop */}
+
+        {
+          showSuccessfullPop === true
+          ?
+          <div
+	className="relative top-20 mx-auto p-5 border w-96 shadow-lg rounded-md bg-white"
+>
+	<div className="mt-3 text-center">
+		<div
+			className="mx-auto flex items-center justify-center h-12 w-12 rounded-full bg-green-100"
+		>
+			<svg
+				className="h-6 w-6 text-green-600"
+				fill="none"
+				stroke="currentColor"
+				viewBox="0 0 24 24"
+				xmlns="http://www.w3.org/2000/svg"
+			>
+				<path
+					stroke-linecap="round"
+					stroke-linejoin="round"
+					stroke-width="2"
+					d="M5 13l4 4L19 7"
+				></path>
+			</svg>
+		</div>
+		<h3 className="text-lg leading-6 font-medium text-gray-900">Successful!</h3>
+		<div className="mt-2 px-7 py-3">
+			<p className="text-sm text-gray-500">
+      registration is Successful
+			</p>
+		</div>
+		<div className="items-center px-4 py-3">
+			<button
+				id="ok-btn"
+				className="px-4 py-2 bg-green-500 text-white text-base font-medium rounded-md w-full shadow-sm hover:bg-green-600 focus:outline-none focus:ring-2 focus:ring-green-300"
+			>
+				OK
+			</button>
+		</div>
+	</div>
+</div>
+
+        : ""}
+
+
+
+
+
+        {/* pop end */}
               </div>
             </div>
           </div>
         </div>
+
         <div className="flex items-center mt-9 w-full">
           <Footer className="bg-gray_800 flex items-center justify-center md:px-5 w-full" />
         </div>
