673b2ada19286f6e717c9e86a4d7e59eb1081c39
shashi@electems.com <shashi@electems.com>
2024-06-14 19:09:21 +0530

    EsLint fixed : List and keys

--- src/functional_components/src copy/ListAndKeys.jsx
+++ src/functional_components/src copy/ListAndKeys.jsx
@@ -1,43 +1,45 @@
-import { useState } from "react";
+import { useState } from 'react';
 const ListAndKeys = () => {
-    const [posts, setPosts] = useState([
-        {
-            id: 1,
-            title: "My First Post",
-            datetime: "July 01, 2021 11:17:36 AM",
-            body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis consequatur expedita, assumenda similique non optio! Modi nesciunt excepturi corrupti atque blanditiis quo nobis, non optio quae possimus illum exercitationem ipsa!"
-        },
-        {
-            id: 2,
-            title: "My 2nd Post",
-            datetime: "July 01, 2021 11:17:36 AM",
-            body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis consequatur expedita, assumenda similique non optio! Modi nesciunt excepturi corrupti atque blanditiis quo nobis, non optio quae possimus illum exercitationem ipsa!"
-        },
-        {
-            id: 3,
-            title: "My 3rd Post",
-            datetime: "July 01, 2021 11:17:36 AM",
-            body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis consequatur expedita, assumenda similique non optio! Modi nesciunt excepturi corrupti atque blanditiis quo nobis, non optio quae possimus illum exercitationem ipsa!"
-        },
-        {
-            id: 4,
-            title: "My Fourth Post",
-            datetime: "July 01, 2021 11:17:36 AM",
-            body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis consequatur expedita, assumenda similique non optio! Modi nesciunt excepturi corrupti atque blanditiis quo nobis, non optio quae possimus illum exercitationem ipsa!"
-        }
-    ])
+  const [posts, setPosts] = useState([
+    {
+      id: 1,
+      title: 'My First Post',
+      datetime: 'July 01, 2021 11:17:36 AM',
+      body: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis consequatur expedita, assumenda similique non optio! Modi nesciunt excepturi corrupti atque blanditiis quo nobis, non optio quae possimus illum exercitationem ipsa!'
+    },
+    {
+      id: 2,
+      title: 'My 2nd Post',
+      datetime: 'July 01, 2021 11:17:36 AM',
+      body: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis consequatur expedita, assumenda similique non optio! Modi nesciunt excepturi corrupti atque blanditiis quo nobis, non optio quae possimus illum exercitationem ipsa!'
+    },
+    {
+      id: 3,
+      title: 'My 3rd Post',
+      datetime: 'July 01, 2021 11:17:36 AM',
+      body: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis consequatur expedita, assumenda similique non optio! Modi nesciunt excepturi corrupti atque blanditiis quo nobis, non optio quae possimus illum exercitationem ipsa!'
+    },
+    {
+      id: 4,
+      title: 'My Fourth Post',
+      datetime: 'July 01, 2021 11:17:36 AM',
+      body: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis consequatur expedita, assumenda similique non optio! Modi nesciunt excepturi corrupti atque blanditiis quo nobis, non optio quae possimus illum exercitationem ipsa!'
+    }
+  ])
 
-    return (
+  return (
         <main>
-            {
-                posts.map((post) => {
-                    return (<div>
-                        {Object.keys(post).map((key) => { return (<div>{key}:{post[key]}</div>) })}
-                    </div>)
-                })
-            }
+            {posts.map((post, postIndex) => (
+                <div key={postIndex}>
+                    {Object.keys(post).map((field, fieldIndex) => (
+                        <div key={fieldIndex}>
+                            {field}: {post[field]}
+                        </div>
+                    ))}
+                </div>
+            ))}
         </main>
-    )
+  );
 }
 
 export default ListAndKeys
