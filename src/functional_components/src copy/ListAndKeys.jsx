09e781d95f0623d45dac5f02d8ee48b2cc73fc44
shashi@electems.com <shashi@electems.com>
2024-06-14 16:07:37 +0530

    List and Keys

---
+++ src/functional_components/src copy/ListAndKeys.jsx
@@ -0,0 +1,43 @@
+import { useState } from "react";
+const ListAndKeys = () => {
+    const [posts, setPosts] = useState([
+        {
+            id: 1,
+            title: "My First Post",
+            datetime: "July 01, 2021 11:17:36 AM",
+            body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis consequatur expedita, assumenda similique non optio! Modi nesciunt excepturi corrupti atque blanditiis quo nobis, non optio quae possimus illum exercitationem ipsa!"
+        },
+        {
+            id: 2,
+            title: "My 2nd Post",
+            datetime: "July 01, 2021 11:17:36 AM",
+            body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis consequatur expedita, assumenda similique non optio! Modi nesciunt excepturi corrupti atque blanditiis quo nobis, non optio quae possimus illum exercitationem ipsa!"
+        },
+        {
+            id: 3,
+            title: "My 3rd Post",
+            datetime: "July 01, 2021 11:17:36 AM",
+            body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis consequatur expedita, assumenda similique non optio! Modi nesciunt excepturi corrupti atque blanditiis quo nobis, non optio quae possimus illum exercitationem ipsa!"
+        },
+        {
+            id: 4,
+            title: "My Fourth Post",
+            datetime: "July 01, 2021 11:17:36 AM",
+            body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis consequatur expedita, assumenda similique non optio! Modi nesciunt excepturi corrupti atque blanditiis quo nobis, non optio quae possimus illum exercitationem ipsa!"
+        }
+    ])
+
+    return (
+        <main>
+            {
+                posts.map((post) => {
+                    return (<div>
+                        {Object.keys(post).map((key) => { return (<div>{key}:{post[key]}</div>) })}
+                    </div>)
+                })
+            }
+        </main>
+    )
+}
+
+export default ListAndKeys
