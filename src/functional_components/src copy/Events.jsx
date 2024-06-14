0ac755993d17381022c80af82cd059ceb90dc98e
shashi@electems.com <shashi@electems.com>
2024-06-14 19:05:41 +0530

    EsLint fixed : Events

--- src/functional_components/src copy/Events.jsx
+++ src/functional_components/src copy/Events.jsx
@@ -1,15 +1,14 @@
-function Events() {
-    
-    const animals = ['zebra', 'horse', 'koala', 'kangaroo','doraemon']
+function Events () {
+  const animals = ['zebra', 'horse', 'koala', 'kangaroo', 'doraemon']
 
-    const animal = ()=>{
-        return animals[Math.trunc(Math.random()*6)]
-    }
+  const animal = () => {
+    return animals[Math.trunc(Math.random() * 6)]
+  }
 
-    function pickAnimal(){
-        document.createElement('h1').write(animal())
-    }
-    
+  function pickAnimal () {
+    document.createElement('h1').write(animal())
+  }
+
   return (
       <div>
         <h1>{animal()}</h1>
